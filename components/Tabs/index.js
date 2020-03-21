5f0a448e912e6d7bbb39fe55803fd56dffbb50b5
John Rossi <DACOOK1983@gamil.com>
2020-03-20 23:39:15 -0400

    mvp complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+
+    .then(response => {
+
+
+        response.data.topics.forEach(item => {
+            tabHolder.append(addComponent(item));
+        })
+    })
+
+    .catch(error => {
+        console.log(`Oops there was an error `, error);
+    })
+
+
+function addComponent(arrayItem) {
+    //create element
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = arrayItem;
+
+
+
+
+    return tab;
+}
+
+const tabHolder = document.querySelector('.topics');
